dfb0ea1f2b710507e3fa3a563f17e76e0b3170bb
ajgahan19 <andregahan77@gmail.com>
2022-12-15 08:41:55 -0800

    final push-please

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -8,37 +8,171 @@
 // Add appropriate dependencies: $ yarn add jest
 
 // Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
+// Run the file with the following command: $ yarn jest 
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+//RANTS:
+// This past weekend I went over the syllabus and watched 8 lectures one of them being "Testing with Jest", Reviwed notes and tired out a couple problems. This problem below I've ran about 9 or 10 different times. 
+// QUESTION: How many functions can you pass within a function within a it block? In the Lecture that was given Elyse talked about passing anonomous functions within a function. I guessing an anonmous functions is when you dont have anything in the parenthesis. 
+//For this problem I will start with a describe statement, I will call it saltySpittune. 
 
+//         const secretCodeWord1 = "Lackadaisical"
+//         // Expected output: "L4ck4d41s1c4l"
+//         const secretCodeWord2 = "Gobbledygook"
+//         // Expected output: "G0bbl3dyg00k"
+//         const secretCodeWord3 = "Eccentric"
+//         // Expected output: "3cc3ntr1c"
+//         
+             //For my first act I will start by sawing a man in half. 
+             // I will start the show with a describe statement, I will call it saltySpittune. 
+            //For my "it" statements function, I will have three expect statements, one per variable...
+            //nothing to hide here folks; other than some broken code that will need to be defined.
+            // lastly, I will use .toEqual as my method. 
+            // We use the method, .toEqual to compaire values. Just like we use Strick Equality operators. ===
+            //Remember: You do not use console log to invoke the function. The function is invoked within the parentheis after, expext.
+//          
+
+//         
+// describe ("saltySpittune", () => {
+//     it("takes in a string and returns a string with a coded message", () => {
+//         expect(saltySpittune(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//         expect(saltySpittune(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//         expect(saltySpittune(secretCodeWord3)).toEqual("3cc3ntr1c")
+//      })
+//     })
+
+
+//     This is a good fail! We are getting some feed back on what needs to be addressed.   
+//                  Reference   Error: saltySpittune is not defined
+
+//     36 |         // Expected output: "3cc3ntr1c"
+//     37 |         //Remember: You invoke the function within the parentheis of 
+//   > 38 |         expect(saltySpittune(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//        |         ^
+//     39 |         expect(saltySpittune(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//     40 |         expect(saltySpittune(secretCodeWord3)).toEqual("3cc3ntr1c")
+//     41 |      })
+
+//     at Object.expect (code-challenges.test.js:38:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.186 s
+// Ran all test suites.
+
+//Since we did not get our function to pass we need to create one that will. 
 // b) Create the function that makes the test pass.
+
+//First thing that comes to mind is a conditional statement per each letter. 
+// I also think a for loop is befitting of a junior bevloper lol. So I shall itterate through the strings characters and identify them. 
+// I think there should be other ways but I might come back and see if I can do better. 
+
+
+// Create a function that takes in a parameter named string.
+// The single parameter will take in a string as an argument.
+// Find a way to iterate through the string.
+// Replace the character with a number. <- I was looking around on google to broaden my understanding of .toEqual then I thought why can I just replace the letters with .replace
+// Return the string 
+
+const morecushinforthe = []
+    for(let i = 0; i < string.length; i++){
+    if (string[i] == "a"){
+        morecushinforthe.push(string[i].replace("a", 4))
+    }
+    else if (string[i] == "e"){
+        morecushinforthe.push(string[i].replace("e", 3))
+    }
+    else if (string[i] == "E"){
+        morecushinforthe.push(string[i].replace("E", 3))
+    }
+    else if (string[i] == "i"){
+        morecushinforthe.push(string[i].replace("i", 1))
+    }
+    else if (string[i] == "o"){
+        morecushinforthe.push(string[i].replace("o", 0))
+    }
+    else {morecushinforthe.push(string[i])}
+}
+    return morecushinforthe.join("")
+}
+
+
+
+
+// const saltySpittune = (string) => {
+//      return (Lackadaisical).split("");
+// }
+//The initial troubleshooting step was to confirm that I converted the string into an array.
+// Expected: "L4ck4d41s1c4l"
+//     Received: ["L", "a", "c", "k", "a", "d", "a", "i", "s", "i", …]
+//     Test Suites: 1 failed, 1 total
+//     Tests:       1 failed, 1 total
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("particularlatters", () => {
+    const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    const letterA = "a"
+    // Expected output: ["Mango", "Apricot", "Peach"]
+    const letterE = "e"
+    // Expected output: ["Cherry", "Blueberry", "Peach"]
+    it("returns an array of all the words containing that particular letter", () => {
+        expect"particularlatters(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect"particularlatters(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+    })
+//This is a good fail 
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+    // ReferenceError: wordSearch is not defined
+//     Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+
 
 // b) Create the function that makes the test pass.
 
+/*
+Create a function named wordSearch.
+The function will take in two parameters named array and letter.
+The argument that will be passed into the parameter array will be an array of words.
+The argument that will be passed into the parameter array will be variable that will contain one character inside of a string.
+Using the .filter() method, you can use the variable as the call back function
+The function will return a new array that contains only the words that match the character
+*/
+// const wordSearch = (array, letter) => {
+//     return array.filter((value) => value.toLowerCase().includes(letter.toLowerCase()))
+// }
+/*
+Test Suites: 1 passed, 1 total
+Tests:       2 passed, 2 total
+Reading the documentation on the .filter() method, there is an example of how to use the method to find a string to uses the filter criteria and push it into a new array.
+Using the .toLowerCase() method, I converted all of the strings inside of the array to lowercase in order to ensure that the search parameter will match the character regardless if it was originally uppercase or lowercase.
+*/
+
+
+
+
+
+REWRITE VARiables ect.
+hit up ruby then push to main 
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
+
 // a) Create a test with expect statements using the variable provided.
+
+/*
+For my tests describe statement I will use the name "fullHouse"
+For the it statement arguement string I will put the end part of the prompt as it is explaining what the test function will do. 
+I will make one expect statement per variable declaration to take in each hand array with numbers in it and it will return me a boolean value of true or false depending on if the "hand" has exactly one pair and a three of a kind. So if it does it will return true stating it is a fullHouse
+*/
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -49,4 +183,53 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+describe("fullHouse", () => {
+    it("function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.", () => {
+      expect(fullHouse(hand1)).toEqual(true)
+      expect(fullHouse(hand2)).toEqual(false)
+      expect(fullHouse(hand3)).toEqual(false)
+      expect(fullHouse(hand4)).toEqual(true)
+    })
+  })
+
+  //    ReferenceError: fullHouse is not defined
+
+
+
 // b) Create the function that makes the test pass.
+
+/* This first method of using for look and conditional at least in this first way was only party effective. only worked for the first and last array. 
+*/
+
+// const fullHouse = (array) => {
+//     for (i=0; i < array.length; i++)
+//     if (array.indexOf(array[i], 3)){
+//     return true
+//     }
+//     else (array.indexOf(array[i], 4))
+//     { 
+//     return false
+//     }
+//  }
+
+/*
+This one Im not really proud of in my head it seemed pretty straight forward. 
+However, when it came to writitng it, I hit a roadblock in how I can articulate what im trying to do. 
+It took some diggin through the sylubus to find something that works.  
+So as you plow through your for loop. + 1 will keep going. 
+*/
+
+const fullHouse = (array) => {
+    let countObj = {}
+    for(let i of array){
+      countObj[i] = (countObj[i] || 0) + 1;
+    }
+
+let vals = Object.values(countObj);
+    if((vals[0] === 2 && vals[1] === 3) || (vals[1] === 2 && vals[0] === 3)){
+      return true;
+    }
+    return false;
+  }
+
+// console.log(fullHouse(hand1));
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,30 +3,121 @@
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
 
-beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# letter_o = 'o'
+# # Expected output: ['coffee', 'soda water']
+# letter_t = 't'
+# Expected output: ['tea', 'water', 'soda water'
 
-letter_o = 'o'
-# Expected output: ['coffee', 'soda water']
-letter_t = 't'
-# Expected output: ['tea', 'water', 'soda water']
+
+#SIDE-NOTE:
+# I had to crack open some of the Ruby lectures and my notes. I am getting everything all mixed up wtih javascript some how. It's been a while and it kind of left my mind a bit.
+
+#PROBLEM:
+# To get started we need to make a function named, ithurts_Alot.
+# The parameters of the function will be named array and letter.
+# The argument that I will pass through is an array of strings and a singular letter inside of a string.
+# We need to Iterate through the array with the letter as a filter...
+# We can use .select! to iniciate a change from the original array. Using this method will filter out only the desired output.
+
+#beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# letter_o = 'o'
+# # Expected output: ['coffee', 'soda water']
+# letter_t = 't'
+# Expected output: ['tea', 'water', 'soda water'
+
+
+
+# def ithurst_Alot array, letter
+#   array.select! {|let| let.include?(letter) } 
+  #the ! character changes the accessor and allows it to mutate.
+# end
+
+# p ithurst_Alot beverages_array, letter_o
+# ["coffee", "soda water"]
+# p ithurst_Alot beverages_array, letter_t
+# ["tea", "water", "soda water"]
+
+
 
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
-# HINT: Google 'ruby get rid of nested arrays'
+# HINT: Google 'ruby get rid of nested arrays' => I didnt see this the first time around and I paid for it the hard way. 
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+# us_states was an undefined local variable and not in my scope since I created a method before I declared it. As always simple mistakes always hurt the most.
+# As of now i've reworked this problem a couple times now and its starting to give me a headache. 
+# I started by creating a method before the array with the hash was defined, I couldn't understand why my method wasn't working when I would pass us_states as the argument. 
+
+# Create a method called thefirst_order and pass in an array as the parameter.
+# I used DOT notation with values to find the values associated with their keys, then flatten the array so they are not embedded arrays in the overall array. 
+# Then, add sort after we flatten so the whole array is being sorted alphabetically!
+
+# us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+
+# def thefirst_order array
+  
+#   array.values.flatten.sort
+# end
+# p thefirst_order(us_states)
+
+
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
+# class Bike 
+#   attr_accessor :model, :wheels, :current_speed
+
+#   def initialize(model)
+#     @model = model
+#     @wheels = 2
+#     @current_speed = 0
+#   end
+#   def bike_info 
+#     "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+#   end
+# end
+# p trek = Bike.new('Trek') 
+# p trek.bike_info
+#<Bike:0x00007fa6ac0b17d8 @model="Trek", @wheels=2, @current_speed=0>
+# "The Trek bike it has two wheels and is going zero mph."
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 
-
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
-
+#I like this one, I haven't played around with code like this since our text based game projects. 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+class Bike 
+  attr_accessor :model, :wheels, :current_speed, :pedal_faster,
+
+  def initialize(model, current_speed)
+    @model = model
+    @wheels = 2
+    @current_speed = 0
+  end
+  def bike_info 
+    "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+  end
+  def pedal_faster(number)
+    @current_speed = number
+    end
+  def brake(number)
+    if @current_speed > number
+    @current_speed = @current_speed - number
+    elsif @current_speed < number
+      @current_speed = 0
+  end
+  end
+end
+
+p my_bike = Bike.new
+p my_bike.pedal_faster(10)
+p my_bike.pedal_faster(28)
+p my_bike.brake(5)
+p my_bike.brake(25)
+
